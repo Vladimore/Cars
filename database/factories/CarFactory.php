546e19ee43cbc8dc1a5419a8a08bc0cfffd4d936
ShowWoodenHorses <vova.vevo666@gmail.com>
2024-06-16 18:10:05 +0300

    add comments

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -17,9 +17,9 @@ class CarFactory extends Factory
      */
 
     private array $images = [
-        '1'=>'https://avtosreda.ru/upload/iblock/ad5/lada-_2_.jpg',
-        '2'=>'https://www.unitauto.ru/media/k2/items/cache/5b98a51d844cf083418c7193dcee292b_XL.jpg',
-        '3'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO_n0NfUbLg1JsrNkzwqeLjtJy0-6X_V7BsA&s'
+        '1'=>'https://avtosreda.ru/upload/iblock/ad5/lada-_2_.jpg', // Lada
+        '2'=>'https://www.unitauto.ru/media/k2/items/cache/5b98a51d844cf083418c7193dcee292b_XL.jpg', // BMW
+        '3'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO_n0NfUbLg1JsrNkzwqeLjtJy0-6X_V7BsA&s' //Focus
     ];
 
     private array $models = [
