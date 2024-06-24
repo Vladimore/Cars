5256b16a2da0e68573078e7a4fa3d876a61a4c2a
ShowWoodenHorses <vova.vevo666@gmail.com>
2024-06-24 20:39:47 +0300

    add command 'ExcelImportCommand' in php artisan

---
+++ app/Console/Commands/ExcelImportCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ExcelImportCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:excel';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'import excel file';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
